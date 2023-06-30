effbc661df4ec24f075c10a25cc0c925e8036b5f
Ujef55 <ujef.kelawala123@gmail.com>
2023-06-30 14:44:18 +0530

    fixing issues

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const addButton = document.querySelector('#addBtn');
+const checkButton = document.querySelector('.check-icon');
+const closeButton = document.querySelector('.close-icon');
 const container3 = document.querySelector('.container3');
 
 
 
 // add button feature
+
+
+// addButton.addEventListener('click', () => {
+//     if(container3.style.display === 'none') {
+//         container3.style.display = 'block';
+//     } else {
+//         container3.style.display = 'none';
+//     }
+// })
+
 addButton.addEventListener('click', () => {
-    if(container3.style.display === 'none') {
-        container3.style.display = 'block';
-    } else {
-        container3.style.display = 'none';
-    }
+    container3.style.display = 'block';
 })
+
+closeButton.addEventListener('click', () => {
+    container3.style.display  = 'none';
+})
+
+
